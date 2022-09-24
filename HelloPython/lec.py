9234e29149a3ded85fefd5b2cb5e488e69b5a97a
Ilya <veruhanov00@mail.ru>
2022-09-24 13:49:14 +0300

    add lec.py1

---
+++ HelloPython/lec.py
@@ -0,0 +1,13 @@
+from re import S
+# value = None
+a = 123
+b = 1.23
+# print (type(a))
+# print(type(b))
+# value = 12345
+# print(type(value))
+s = 'hello world'
+print (s)
+print(a,'-', b,'-' ,s)
+print('{} - {} - {}'.format(a,b,s))
+print(f'{a} - {b} - {s}')
