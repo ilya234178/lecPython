dffb953906f8b03cea584b766ebf83d442787688
Ilya <veruhanov00@mail.ru>
2022-09-25 21:33:31 +0300

    Add program

--- HelloPython/lec.py
+++ HelloPython/lec.py
@@ -6,8 +6,20 @@ b = 1.23
 # print(type(b))
 # value = 12345
 # print(type(value))
-s = 'hello world'
-print (s)
-print(a,'-', b,'-' ,s)
-print('{} - {} - {}'.format(a,b,s))
-print(f'{a} - {b} - {s}')
+# s = 'hello world'
+# print (s)
+# print(a,'-', b,'-' ,s)
+# print('{1} - {2} - {0}'.format(a,b,s))
+# print(f'{a} - {b} - {s}')
+# f = False
+# print(f)
+# list = [1,2,3]
+# print(list)
+
+print('Введите a');
+a = int(input())
+print('Введите b');
+b = int(input())
+print(a, ' + ' ,b, ' = ', a+b)
+# print('{} {}'.format(a,b))
+# print(f'{a} {b}')
