cfee584451aa074119574ac7e4f3c8f235944e03
Ilya <veruhanov00@mail.ru>
2022-10-13 18:39:37 +0300

    add list min, max

---
+++ Новая папка/PYTHON_CODE/list1.py
@@ -0,0 +1,6 @@
+
+shopping_list = ['pens', 'bugs', 'box', 'mask', 'dog']
+
+print(len(shopping_list))
+print(max(shopping_list)) #возвращает последнюю строку 
+print(min(shopping_list)) #возвращает строку которая была бы первой в алфавитном порядке
