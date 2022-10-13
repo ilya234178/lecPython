1acded7c2c812d8636adf6383f75e9cd70b7a971
Ilya <veruhanov00@mail.ru>
2022-10-13 21:23:59 +0300

    add method items

---
+++ Новая папка/PYTHON_CODE/method_items.py
@@ -0,0 +1,4 @@
+# метод items используется для возврата кортежей с парами всех ключей и значений словаря
+products = {'Fresh ott': 500, 'xango juice': 30, '5 star chocolate': 20, 'dan frutt cake': 50 }
+itemlist = products.items()
+print('the output of items() method : \n', itemlist)
